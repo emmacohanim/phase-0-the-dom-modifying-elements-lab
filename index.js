eba1c3cc986887b56395b17c11709d6c85a2106c
Emma Cohanim <emmacohanim@gmail.com>
2022-07-06 13:18:55 -0700

    done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+var element = document.getElementById("main");
+element.parentNode.removeChild(element)
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Emma is the champion"
